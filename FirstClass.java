52962d83cc8994731c956bcf73b482706b41305d
Dipanjali <122343977+DipanjaliPawar18@users.noreply.github.com>
2024-08-31 13:20:00 +0530

    Add files via upload
    First program that shows how to display output in different formates.

---
+++ FirstClass.java
@@ -0,0 +1,12 @@
+class FirstClass{
+    public static void main(String[] args) {
+        System.out.print("Hello World!!");
+        System.out.print("Hello World!!");
+
+        System.out.println("\n Hello World!!");
+        System.out.println("Hello World!!");
+
+        System.out.print("Hello World!! \n");
+        System.out.print("Hello World!! \n");
+    }
+}
