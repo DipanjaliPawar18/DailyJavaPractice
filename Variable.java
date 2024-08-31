5969474fd247ef56559f88d00c33415459d830a5
Dipanjali <122343977+DipanjaliPawar18@users.noreply.github.com>
2024-08-31 13:23:45 +0530

    Add files via upload
    Integer variable in java

---
+++ Variable.java
@@ -0,0 +1,26 @@
+class Variable{
+    public static void main(String[] args) {
+        //variables in java
+
+        // Primitive - byte short char boolean int long float double
+        // Non-Primitive - string array class object interface
+
+        int a = 15;
+        int b = 20;
+
+        int sum = a+b;
+
+        System.out.println(sum);
+
+        int diff = b-a;
+
+        System.out.println(diff);
+
+        int mul = a * b;
+        
+        System.out.println(mul);
+
+        System.out.println(mul/diff);
+
+    }
+}
